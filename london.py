7b224faa370e6c21e007831fc71dee5e5a39a741
Martin Allen <lefthand.dark@gmail.com>
2021-01-13 23:06:47 -0500

    Data update

--- london.py
+++ london.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from london_scrape import scrape
-from thumbs import make_thumbnail
-from tweet import send_tweet
+# from thumbs import make_thumbnail
+# from tweet import send_tweet
 
 '''
 Checks MLHU for updates. 
@@ -13,11 +13,11 @@ and tweets.
 data = scrape()
 
 # updates only if data is new
-if data:
-    print('Data is new.')
-    print('Making thumbnail...')
-    make_thumbnail()
-    print('Sending tweet...')
-    send_tweet()
-else:
-    print('Data is same.')
+# if data:
+#     print('Data is new.')
+#     print('Making thumbnail...')
+#     make_thumbnail()
+#     print('Sending tweet...')
+#     send_tweet()
+# else:
+#     print('Data is same.')
